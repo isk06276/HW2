0a731e19540faa18e924141d355c639c20aaaada
isk06276 <isk06276@gmail.com>
2021-05-14 20:02:15 +0900

    set_priority

--- thread.c
+++ thread.c
@@ -343,7 +343,10 @@ thread_foreach (thread_action_func *func, void *aux)
 void
 thread_set_priority (int new_priority) 
 {
-  thread_current ()->priority = new_priority;
+  if(!thread_mlfqs)
+    {
+      thread_current ()->priority = new_priority;
+    }
 }
 
 /* Returns the current thread's priority. */
