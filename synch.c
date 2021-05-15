b313d2fa442f6e387c6b9c64caa36b41084f7834
Yohan-ParkGist <johnyein@gm.gist.ac.kr>
2021-05-15 19:58:37 +0900

    sema down change

--- synch.c
+++ synch.c
@@ -68,7 +68,8 @@ sema_down (struct semaphore *sema)
   old_level = intr_disable ();
   while (sema->value == 0) 
     {
-      list_push_back (&sema->waiters, &thread_current ()->elem);
+      //기존의 list_push_back를 list_insert_ordered로 교체한다.
+      list_insert_ordered (&sema->waiters, &thread_current()->elem, priority_compare, NULL);
       thread_block ();
     }
   sema->value--;
