fe9fbff722e668bb1b29e45f195bca16f8f9fb1e
isk06276 <isk06276@gmail.com>
2021-05-16 21:07:54 +0900

    add comment

--- synch.h
+++ synch.h
@@ -41,7 +41,9 @@ void cond_wait (struct condition *, struct lock *);
 void cond_signal (struct condition *, struct lock *);
 void cond_broadcast (struct condition *, struct lock *);
 
+//pintos HW2 function
 bool compare_priority_semaphore (const struct list_elem* a, const struct list_elem* b, void* aux);
+
 /* Optimization barrier.
 
    The compiler will not reorder operations across an
